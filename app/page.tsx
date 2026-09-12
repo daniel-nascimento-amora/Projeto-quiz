0648736443b4f4b81f9612b24839263fb22932ee
daniel-nascimento-amora <daniel_amora@hotmail.com>
2026-09-12 15:34:02 -0300

    criando css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import cardStyles from "./layout";
+
+
+
 export default function Page() {
   return (
     
-      <h1>Welcome to the Quiz Project!</h1>
+      <main>
+        <div className = {cardStyles.card} >
+          <header>
+            <h1>Teste suas Habilidades</h1>
+            </header>
+            <p>Teste seus conhecimentos sobre temas variados</p>
+            <p>FORMULÁRIO/BOTÃO</p>
+            <footer><p>
+              Criado por Daniel Amora
+            </p></footer>
+            </div>
+      </main>
+
+
          
   );
 }
