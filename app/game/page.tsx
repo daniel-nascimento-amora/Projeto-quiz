704c02e1323c81cbdd8e8f14d49f0842e9b4cdeb
daniel-nascimento-amora <daniel_amora@hotmail.com>
2026-09-15 17:18:33 -0300

    melhorando sub página game

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,8 +1,28 @@
+import QuizLogo from "../components/quizlogo";
+import Card from "../components/Card";
+import pageStyles from "../page.module.css";
+
 export default function GameScreen() {
   return (
-    <div>
-      <h1>Game Page</h1>
-      <p>Welcome to the game page!</p>
-    </div>
+    <main className={pageStyles.screen} style={{ flex: 1 }}>
+      <section className={pageStyles.container}>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "right",
+            marginBottom: "24px"
+          }}
+        >
+          
+        </div>
+        <QuizLogo />
+      <Card
+        headerTitle="Pergunta 1 de 5"
+      >
+        Pergunta
+      </Card>
+      </section>
+    </main>
   );
 }
