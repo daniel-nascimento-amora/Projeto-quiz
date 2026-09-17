9d53110666818c1e22224b1cde5fe9203be45eb3
daniel-nascimento-amora <daniel_amora@hotmail.com>
2026-09-17 17:46:17 -0300

    criando o config.json

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -2,18 +2,11 @@ import QuizLogo from "../components/quizlogo";
 import Card from "../components/Card";
 import pageStyles from "../page.module.css";
 import { Footer } from "../components/Footer";
+import config from "../../config.json";  
+
+const questions = config.questions;
 
 
-const questions = [
-  {
-  image: "https://sharpmindlogic.com/wp-content/uploads/2026/09/bgproje.jpg",
-  title: "Qual é a capital da França?",
-  description: "Escolha a resposta correta entre as opções abaixo.",
-  alternatives: ["Paris", "Londres", "Berlim", "Madrid"]
-}
-
-  
-];
 
 export default function GameScreen() {
   const currentQuestion = 0;
